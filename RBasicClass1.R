e1683248ad1c22d7778f7387d1988712b2363c07
karisuma23 <96929176+karisuma23@users.noreply.github.com>
2022-04-18 10:33:22 +0900

    rr

---
+++ RBasicClass1.R
@@ -0,0 +1,19 @@
+dim(available.packages())
+
+sessionInfo()
+
+install.packages("stringr")
+
+library(stringr)
+
+search() # 현재 로드된 패키지 확인 
+
+remove.packages("stringr")
+
+data()
+
+hist(Nile)
+
+# 변수
+
+
